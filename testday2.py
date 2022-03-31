6896ec8c8cda96dcfa48fef12013dfaec113ab7a
zhixiyao <yaozzzack@gmail.com>
2022-03-31 15:52:05 -0500

    Create testday2.py

---
+++ testday2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 31 15:43:52 2022
+
+@author: zoujingwen
+"""
+
+print('adorable wenwen')
+print('stupid xixi')
+print('ugly wenwen')
+print('handsome xixi')
